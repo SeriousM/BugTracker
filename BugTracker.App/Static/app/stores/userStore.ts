b186245db4733e539a6bfa2dd1a1efd13428bd1c
Bernhard Millauer <bernhard.millauer@gmail.com>
2016-01-27 12:34:41 +0100

    Fixing the import of the angular2-redux-store.

--- BugTracker.App/Static/app/stores/userStore.ts
+++ BugTracker.App/Static/app/stores/userStore.ts
@@ -1,9 +1,8 @@
 ///<reference path='../../../node_modules/immutable/dist/immutable.d.ts'/>
-///<reference path='../../../node_modules/angular2-redux-store/lib/index.d.ts'/>
 import { createStore } from "redux";
 import { Injectable } from "angular2/core";
 import { Record } from "immutable";
-import { ReduxStore } from "angular2-redux-store";
+import { ReduxStore } from "angular2-redux-store/lib";
 import { Actions } from "./userActions";
 
 export const userStoreReducer = (state:any = {}, action:any) => {
