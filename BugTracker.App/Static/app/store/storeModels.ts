f60c4453fe9ff931906e251a9089a940b31b69b5
SeriousM <bernhard.millauer@gmail.com>
2016-02-12 22:16:02 +0100

    Fixing the immutable models by defining the properties explicitily

--- BugTracker.App/Static/app/store/storeModels.ts
+++ BugTracker.App/Static/app/store/storeModels.ts
@@ -2,11 +2,16 @@ import { Record, List } from 'immutable';
 
 // http://blog.jhades.org/angular-2-application-architecture-building-flux-like-apps-using-redux-and-immutable-js-js/
 
+// Note: the properties have to be defined as normal public properties, not as public parameters for the constructor.
+// Otherwise you will face an "Cannot set on an immutable record." error.
+
 const UserModelRecord = Record({
     name: <string>null
 });
 export class UserModel extends UserModelRecord {
-    constructor(public name: string) {
+    public name: string;
+    
+    constructor(name: string) {
         super({ name });
     }
 }
@@ -15,7 +20,9 @@ const IssueModelRecord = Record({
     title: <string>null
 });
 export class IssueModel extends IssueModelRecord {
-    constructor(public title: string) {
+    public title: string;
+    
+    constructor(title: string) {
         super({ title });
     }
 }
@@ -26,8 +33,9 @@ const CurrentUserStateRecord = Record({
 });
 export class CurrentUserState extends CurrentUserStateRecord {
     public isSet: boolean;
+    public user: UserModel;
 
-    constructor(public user?: UserModel) {
+    constructor(user?: UserModel) {
         super({
             user,
             isSet: user != null
