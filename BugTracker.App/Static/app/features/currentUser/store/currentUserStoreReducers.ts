63497482efc7b8d83a23056d2525286798543163
Bernhard Millauer <bernhard.millauer@gmail.com>
2016-02-15 11:43:41 +0100

    The models now have set-methods

--- BugTracker.App/Static/app/features/currentUser/store/currentUserStoreReducers.ts
+++ BugTracker.App/Static/app/features/currentUser/store/currentUserStoreReducers.ts
@@ -3,7 +3,7 @@ import { UserModel, CurrentUserState } from "../../../store/storeModels";
 import { CurrentUserStoreActionTypes, CurrentUserStoreActions, ISetCurrentUserAction, IRemoveCurrentUserAction } from "./currentUserStoreActions";
 
 const setCurrentUser = (state: CurrentUserState, action: ISetCurrentUserAction): CurrentUserState => {
-    return new CurrentUserState(action.user);
+    return state.setUser(action.user);
 }
 const removeCurrentUser = (state: CurrentUserState, action: IRemoveCurrentUserAction): CurrentUserState => {
     return new CurrentUserState();
