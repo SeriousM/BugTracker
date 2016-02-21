b8ebf0f9e4ebee2f9033f53b6687a40555be8a41
SeriousM <bernhard.millauer@gmail.com>
2016-02-22 00:57:44 +0100

    Adding the models.ts file to have all models in one place for reference

---
+++ BugTracker.App/Static/app/models/models.ts
@@ -0,0 +1,5 @@
+var x = 1;
+export { IssueModel } from './issueModel';
+export { PermissionModel } from './permissionModel';
+export { RegisterUserModel } from './registerUserModel';
+export { UserModel } from './userModel';
