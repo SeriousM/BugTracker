8f191de0071584adc3f42c0efcc82f1056fe25e2
lexon0011 <martin.schagerl@lexon.at>
2016-02-23 09:53:09 +0100

    add return type attributes

---
+++ BugTracker.App/Attributes/ReturnsModelAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BugTracker.App.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class ReturnsModelAttribute : Attribute
+    {
+        public ReturnsModelAttribute(string modelType)
+        {
+        }
+    }
+}
---
+++ BugTracker.App/Attributes/ReturnsModelsAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BugTracker.App.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class ReturnsModelsAttribute : Attribute
+    {
+        public ReturnsModelsAttribute(Enum listType, string modelType)
+        {
+        }    
+    }
+}
