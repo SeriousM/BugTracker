c46415bf2520bdd5d72ea8165cec3c0a8ba5e8f3
lexon0011 <martin.schagerl@lexon.at>
2016-02-23 10:49:30 +0100

    change attribute parameter to the correct Enum Type

--- BugTracker.App/Attributes/ReturnsModelsAttribute.cs
+++ BugTracker.App/Attributes/ReturnsModelsAttribute.cs
@@ -5,7 +5,7 @@ namespace BugTracker.App.Attributes
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public class ReturnsModelsAttribute : Attribute
     {
-        public ReturnsModelsAttribute(Enum listType, string modelType)
+        public ReturnsModelsAttribute(TypescriptIterable listType, string modelType)
         {
         }    
     }
