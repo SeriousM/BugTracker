a3c5fcdd15142e7738b55025858ad21d72bd93d3
SeriousM <bernhard.millauer@gmail.com>
2016-02-25 13:22:05 +0100

    first settigns for the router
    ..but far away from done

--- BugTracker.App/Static/app/app.ts
+++ BugTracker.App/Static/app/app.ts
@@ -1,37 +1,39 @@
 ﻿import { Component, provide } from "angular2/core";
 import { HTTP_PROVIDERS, RequestOptions  } from 'angular2/http';
 import { bootstrap } from "angular2/platform/browser";
+
+import { Router, RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, LocationStrategy, APP_BASE_HREF } from "angular2/router";
+
 import { DefaultRequestOptions } from "./services/service.base";
 import { AppStore, appStoreFactory } from "./store/appStore";
 import { AppConfiguration } from './config/config.base';
 
-import { CurrentUserState } from "./models/models";
 import { APP_WEBSERVICES } from "./services/services"
 
+import { AppHeaderComponent } from "./features/common/view/appHeaderComponent";
 import { UserLogin } from "./features/currentUser/view/userLoginComponent";
-import { UserAvatar } from "./features/currentUser/view/userAvatarComponent";
 import { IssuesContainer } from "./features/issues/view/issuesContainerComponent";
+
+@RouteConfig([
+    { path: '/login', name: 'Login', component: UserLogin, useAsDefault: true },
+    { path: '/issues', name: 'Issues', component: IssuesContainer }
+])
 
 @Component({
     selector: "bug-tracker",
-    directives: [UserLogin, UserAvatar, IssuesContainer],
+    directives: [AppHeaderComponent],
     template: `
         <div>
-            <user-login *ngIf="currentUser.user == null"></user-login>
-            <user-avatar *ngIf="currentUser.user != null"></user-avatar>
-            <issues-container *ngIf="currentUser.user != null"></issues-container>
+            <app-header></app-header>
+            
+            <!-- Routed views go here -->
+            <router-outlet></router-outlet>
         </div>
     `
 })
 
 export class App {
-    public currentUser: CurrentUserState;
     constructor(private appStore: AppStore) {
-        appStore.subscribe(this.appStoreUpdate.bind(this));
-        this.appStoreUpdate();
-    }
-    private appStoreUpdate() {
-        this.currentUser = this.appStore.getState().currentUser;
     }
 }
 
@@ -40,6 +42,8 @@ var appConfiguration = new AppConfiguration((<any>window).appConfiguration);
 bootstrap(App, [
     HTTP_PROVIDERS,
     APP_WEBSERVICES,
+    ROUTER_PROVIDERS,
+    provide(APP_BASE_HREF, { useValue: '/static' }),
     provide(AppConfiguration, { useValue: appConfiguration }),
     provide(RequestOptions, { useClass: DefaultRequestOptions }),
     provide(AppStore, { useFactory: appStoreFactory })
