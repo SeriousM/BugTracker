b4c3970bc6f869e1ed6f376fd96fd36ae813efaa
lexon0011 <martin.schagerl@lexon.at>
2016-03-02 15:32:27 +0100

    Expand WebApi to update issue models

--- BugTracker.Data/Repositories/Abstract/IIssueAccess.cs
+++ BugTracker.Data/Repositories/Abstract/IIssueAccess.cs
@@ -8,6 +8,7 @@ namespace BugTracker.Data.Repositories.Abstract
     public interface IIssueAccess
     {
         Issue Add(Guid userId, string title, string content);
+        void Update(Guid issueId, string title, string content, bool isClosed);
         List<Issue> GetAllByUserId(Guid userId);
     }
 }
